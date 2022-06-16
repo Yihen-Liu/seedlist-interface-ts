22cbf458b9a87ac27b825365f2db83b8889487c3
Yihen <liuyihen@gmail.com>
2022-06-16 18:20:13 +0800

    add encrypto process jpg and html show

---
+++ src/pages/docs/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {Box, Center, Container, HStack, Text} from "@chakra-ui/layout";
+import { IBaseProps } from "../../interfaces/props";
+import {Image} from "@chakra-ui/react";
+import {Trans} from "@lingui/macro";
+
+const Document:React.FC<IBaseProps> = (props:IBaseProps)=>{
+	return(
+			<Center>
+					<Box>
+						<Center>
+							<Image marginY="10px" boxShadow="1024" src="./media/encrypt-flow.png" />
+						</Center>
+						<Center>
+							<Text>seedlist-encrypt-process.jpg</Text>
+						</Center>
+					</Box>
+
+
+			</Center>
+);
+}
+
+export {Document};
